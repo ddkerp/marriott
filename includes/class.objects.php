3d969b7ceed0a05c39ddeff9e568cc0e7761c4a5
Dilipkumar Durairajan <ddkerp1@gmail.com>
2017-01-03 09:41:48 +0530

    admin

--- includes/class.objects.php
+++ includes/class.objects.php
@@ -16,3 +16,11 @@
             parent::__construct('newsletter', array('email'), $id);
         }
     }
+	
+	class Review extends DBObject
+    {
+        public function __construct($id = null)
+        {
+            parent::__construct('review', array('id','venue_id','user_name','email','phone','review_text','ratings','status'), $id);
+        }
+    }
