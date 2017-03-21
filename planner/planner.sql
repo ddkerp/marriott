bb129093a9d8c781084faef05e9e3ff757e75d72
Dilipkumar Durairajan <ddkerp1@gmail.com>
2017-03-21 20:10:58 +0530

    Update planner.sql

--- planner/planner.sql
+++ planner/planner.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2017 at 04:58 PM
+-- Generation Time: Mar 21, 2017 at 03:40 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -26,22 +26,62 @@ SET time_zone = "+00:00";
 -- Table structure for table `planner`
 --
 
-CREATE TABLE IF NOT EXISTS  `planner` (
+DROP TABLE IF EXISTS `planner`;
+CREATE TABLE IF NOT EXISTS `planner` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `groom_name` varchar(255) NOT NULL,
+  `bride_name` varchar(255) NOT NULL,
+  `groom_pimage` varchar(255) NOT NULL,
+  `bride_pimage` varchar(255) NOT NULL,
+  `event_date` date NOT NULL,
+  `header_image` varchar(255) NOT NULL,
+  `event_name` varchar(255) NOT NULL,
+  `bride_description` text NOT NULL,
+  `groom_description` text NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `published_date` datetime NOT NULL,
+  `ip_address` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
- `id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
- `url` VARCHAR( 255 ) NOT NULL ,
- `groom_name` VARCHAR( 255 ) NOT NULL ,
- `bride_name` VARCHAR( 255 ) NOT NULL ,
- `groom_pimage` VARCHAR( 255 ) NOT NULL ,
- `bride_pimage` VARCHAR( 255 ) NOT NULL ,
- `event_date` DATE NOT NULL ,
- `session_id` VARCHAR( 255 ) NOT NULL ,
- `status` TINYINT( 1 ) NOT NULL ,
- `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
- `published_date` DATETIME NOT NULL ,
- `ip_address` VARCHAR( 255 ) NOT NULL ,
-PRIMARY KEY (  `id` )
-) ENGINE = INNODB DEFAULT CHARSET = latin1 AUTO_INCREMENT =1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `planner_gallery`
+--
+
+DROP TABLE IF EXISTS `planner_gallery`;
+CREATE TABLE IF NOT EXISTS `planner_gallery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `planner_id` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `image_title` varchar(255) NOT NULL,
+  `image_description` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `planner_guest`
+--
+
+DROP TABLE IF EXISTS `planner_guest`;
+CREATE TABLE IF NOT EXISTS `planner_guest` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `planner_id` int(11) NOT NULL,
+  `guest_name` varchar(255) NOT NULL,
+  `guest_image` varchar(255) NOT NULL,
+  `guest_relation` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
