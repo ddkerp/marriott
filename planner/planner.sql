fa13689d88467e7cc38b687698fdf7b4993432e5
Dilipkumar Durairajan <ddkerp1@gmail.com>
2017-03-21 07:21:49 +0530

    Update planner.sql

--- planner/planner.sql
+++ planner/planner.sql
@@ -26,21 +26,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `planner`
 --
 
-CREATE TABLE IF NOT EXISTS `planner` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
-  `groom_name` varchar(255) NOT NULL,
-  `bride_name` varchar(255) NOT NULL,
-  `groom_pimage` varchar(255) NOT NULL,
-  `bride_pimage` varchar(255) NOT NULL,
-  `event_date` date NOT NULL,
-  `session_id` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `published_date` datetime NOT NULL,
-  `ip_address` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS  `planner` (
+
+ `id` INT( 11 ) NOT NULL AUTO_INCREMENT ,
+ `url` VARCHAR( 255 ) NOT NULL ,
+ `groom_name` VARCHAR( 255 ) NOT NULL ,
+ `bride_name` VARCHAR( 255 ) NOT NULL ,
+ `groom_pimage` VARCHAR( 255 ) NOT NULL ,
+ `bride_pimage` VARCHAR( 255 ) NOT NULL ,
+ `event_date` DATE NOT NULL ,
+ `session_id` VARCHAR( 255 ) NOT NULL ,
+ `status` TINYINT( 1 ) NOT NULL ,
+ `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+ `published_date` DATETIME NOT NULL ,
+ `ip_address` VARCHAR( 255 ) NOT NULL ,
+PRIMARY KEY (  `id` )
+) ENGINE = INNODB DEFAULT CHARSET = latin1 AUTO_INCREMENT =1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
